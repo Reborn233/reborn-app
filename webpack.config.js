e2422d01ef7729f07ba8594db2e4d3ebd9e73dbe
Reborn233 <499289113@qq.com>
2021-03-21 17:27:57 +0800

    Update webpack.config.js
    1

--- webpack.config.js
+++ webpack.config.js
@@ -58,8 +58,8 @@ module.exports = (options = {}) => ({
         }
       ],
       ios: {
-        'apple-mobile-web-app-title': 'AppTitle',
-        'apple-mobile-web-app-status-bar-style': 'black'
+        'apple-mobile-web-app-title': 'Reborn',
+        'apple-mobile-web-app-status-bar-style': 'default'
       }
     }),
     new GenerateSW({
